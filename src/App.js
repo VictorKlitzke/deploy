e12572d08542b57e5dffcda71c9148cb15006423
Victor Klitzke <victorklitzke2@gmail.com>
2025-01-06 22:16:14 -0300

    feature: deploy

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ const authRouters = require('../routers/index');
 const cookieParser = require('cookie-parser');
 
 const corsOptions = {
-  origin: 'http://172.20.10.10:3000',
+  origin: '*',
   credentials: true,
   optionsSuccessStatus: 200,
 };
